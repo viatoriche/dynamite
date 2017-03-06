169489825df0b354298863a1eec4b81b7e3888fb
viatoriche / Maxim Panfilov <Maxim.Panfilov@acronis.com>
2017-03-06 19:09:21 +0300

    add codeclimate-test-reporter to test requirements and fix test runner

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-nosetests dynamite --with-coverage --cover-package=dynamite --cover-erase $@
+AWS_ACCESS_KEY_ID=123 AWS_SECRET_ACCESS_KEY=123 nosetests dynamite --with-coverage --cover-package=dynamite --cover-erase $@
 flake8 dynamite --ignore=E128,E501
 
 if [[ ! "$@" ]] ; then
